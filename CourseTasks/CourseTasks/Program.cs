0b4f901e15143b1a4fa82c707423fa3956877091
Анна Титова <lisa@ngs.ru>
2017-05-09 17:08:31 +0700

    Hello World

--- CourseTasks/CourseTasks/Program.cs
+++ CourseTasks/CourseTasks/Program.cs
@@ -10,7 +10,8 @@ namespace CourseTasks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HW!") ;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
