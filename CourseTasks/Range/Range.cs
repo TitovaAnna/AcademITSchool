466e1ac284de446f180eff3f485c5c43bb22d5d7
Анна Титова <lisa@ngs.ru>
2017-05-19 13:57:07 +0700

    Выделение класса Range в отдельный файл, изменение методов.

---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        private double from;
+        private double to;
+
+        public Range(double from, double to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+        public double Length
+        {
+            get { return to - from; }
+        }
+        public double From
+        {
+            get { return from; }
+            set { from = value; }
+        }
+        public double To
+        {
+            get { return to; }
+            set { to = value; }
+        }
+
+        public bool IsInside(double number)
+        {
+            return (number <= to) && (number >= from);
+        }
+
+        public Range GetCrossing(Range range)
+        {
+
+            if ((From > range.To) || (range.From > To))
+            {
+                return null;
+            }
+            else
+            {
+                double rangeNewFrom = (From > range.From) ? From : range.From;
+                double rangeNewTo = (To < range.To) ? To : range.To;
+                return new Range(rangeNewFrom, rangeNewTo);
+            }
+        }
+
+
+        public Range[] GetCombination(Range range)
+        {
+
+            if ((From > range.To) || (range.From > To))
+            {
+                Range[] newRangeArray = { this, range };
+                return newRangeArray;
+            }
+            else
+            {
+                double min = (From < range.From) ? From : range.From;
+                double max = (To > range.To) ? To : range.To;
+                Range newRange = new Range(min, max);
+                Range[] newRangeArray = { newRange };
+                return newRangeArray;
+            }
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+
+            if ((From > range.To) || (range.From > To))
+            {
+                Range[] newRangeArray = { this };
+                return newRangeArray;
+            }
+            else if ((To > range.To) && (From < range.From))
+            {
+                Range[] newRangeArray = { new Range(From, range.From), new Range(range.To, To) };
+                return newRangeArray;
+            }
+            else if ((To < range.To) && (From > range.From))
+            {
+                return null;
+            }
+            else
+            {
+                double min = (From < range.From) ? From : range.From;
+                double max = (To > range.To) ? range.To : To;
+                Range[] newRangeArray = { new Range(min, max) };
+                return newRangeArray;
+            }
+        }
+
+    }
+}
