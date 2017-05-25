8b905e4a7970ea4d7cf782ad878eb681c2bc955d
Анна Титова <lisa@ngs.ru>
2017-05-25 16:10:07 +0700

    Первая доработка Sort

--- CourseTasks/Sort/Program.cs
+++ CourseTasks/Sort/Program.cs
@@ -9,101 +9,35 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            if (args.Length < 3)
+            if (args.Length < 4)
             {
                 Console.WriteLine("Не хватает аргументов командной строки");
                 Console.ReadKey();
-            }
-            else
-            {
-                int position = args[2].IndexOf("txt");
-
-                if (args[2][position + 6].Equals('a') && args[2][position + 4].Equals('i'))
-                {
-                    Sort(args[1], args[2].Remove(position + 3), true);
-                }
-                else if (args[2][position + 6].Equals('d') && args[2][position + 4].Equals('i'))
-                {
-                    Sort(args[1], args[2].Remove(position + 3), false);
-
-                }
-                else if (args[2][position + 4].Equals('s'))
-                {
-                    Sort(args[1], args[2].Remove(position + 3));
-                }
-
-            }
-
-        }
-
-        public static void Sort(string fileNameIn, string fileNameOut, bool increas)
-        {
-            try
-            {
-
-                int[] arrayDigital;
-                List<string> arrayLine = new List<string>();
-
-                using (StreamReader reader = new StreamReader(fileNameIn))
-                {
-                    int i = 0;
-                    while (!reader.EndOfStream)
-                    {
-                        arrayLine.Insert(i, reader.ReadLine());
-                        Console.WriteLine(arrayLine[i]);
-                        Console.ReadKey();
-                        i++;
-                    }
-                }
-
-                arrayDigital = Array.ConvertAll(arrayLine.ToArray(), Convert.ToInt32);
-
-                for (int i = 1; i < arrayDigital.Length; i++)
-                {
-                    int temp = arrayDigital[i];
-                    int j = i - 1;
-                    if (increas)
-                    {
-                        while ((j >= 0) && (arrayDigital[j] > temp))
-                        {
-                            arrayDigital[j + 1] = arrayDigital[j];
-                            j--;
-                        }
-                        arrayDigital[j + 1] = temp;
-                    }
-                    else
-                    {
-                        while ((j >= 0) && (arrayDigital[j] < temp))
-                        {
-                            arrayDigital[j + 1] = arrayDigital[j];
-                            j--;
-                        }
-                        arrayDigital[j + 1] = temp;
-                    }
-
-                }
-
-                using (StreamWriter writer = new StreamWriter(fileNameOut))
-                {
-                    foreach (int i in arrayDigital)
-                    {
-                        writer.WriteLine(i);
-                    }
-
-                }
-            }
-            catch (Exception exeption)
-            {
-                Console.WriteLine(exeption.Message);
-                Console.ReadKey();
                 return;
             }
+
+            string[] arrayLine = ReadFile(args[0]);
+
+            if (arrayLine == null)
+            {
+                return;
+            }
+
+            arrayLine = Sort(arrayLine, args[2].ToLower(), args[3].ToLower());
+
+            if (arrayLine == null)
+            {
+                return;
+            }
+            WriteFile(args[1], arrayLine);
         }
-        public static void Sort(string fileNameIn, string fileNameOut)
+
+        public static string[] ReadFile(string fileNameIn)
         {
             try
             {
                 List<string> arrayLine = new List<string>();
+
                 using (StreamReader reader = new StreamReader(fileNameIn, Encoding.Default))
                 {
                     int i = 0;
@@ -113,18 +47,106 @@ namespace Sort
                         i++;
                     }
                 }
-                for (int i = 1; i < arrayLine.Count; i++)
-                {
-                    string temp = arrayLine[i];
-                    int j = i - 1;
-                    while ((j >= 0) && (arrayLine[j].CompareTo(temp) > 0))
-                    {
-                        arrayLine[j + 1] = arrayLine[j];
-                        j--;
-                    }
-                    arrayLine[j + 1] = temp;
-                }
 
+                return arrayLine.ToArray();
+            }
+
+            catch (Exception exeption)
+            {
+                Console.WriteLine(exeption.Message);
+                Console.ReadKey();
+                return null;
+            }
+        }
+        public static string[] Sort(string[] arrayLine, string type, string order)
+        {
+            try
+            {
+                if (type.Equals("-i"))
+                {
+                    int[] arrayDigital = Array.ConvertAll(arrayLine, Convert.ToInt32);
+
+                    for (int i = 1; i < arrayDigital.Length; i++)
+                    {
+                        int temp = arrayDigital[i];
+                        int j = i - 1;
+                        if (order.Equals("-a"))
+                        {
+                            while ((j >= 0) && (arrayDigital[j] > temp))
+                            {
+                                arrayDigital[j + 1] = arrayDigital[j];
+                                j--;
+                            }
+                            arrayDigital[j + 1] = temp;
+                        }
+                        else if (order.Equals("-d"))
+                        {
+                            while ((j >= 0) && (arrayDigital[j] < temp))
+                            {
+                                arrayDigital[j + 1] = arrayDigital[j];
+                                j--;
+                            }
+                            arrayDigital[j + 1] = temp;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не правильно задан порядок сортировки");
+                            Console.ReadKey();
+                            return null;
+                        }
+                    }
+                    return Array.ConvertAll(arrayDigital, Convert.ToString);
+                }
+                else if (type.Equals("-s"))
+                {
+                    for (int i = 1; i < arrayLine.Length; i++)
+                    {
+                        string temp = arrayLine[i];
+                        int j = i - 1;
+
+                        if (order.Equals("-a"))
+                        {
+                            while ((j >= 0) && (arrayLine[j].CompareTo(temp) > 0))
+                            {
+                                arrayLine[j + 1] = arrayLine[j];
+                                j--;
+                            }
+                            arrayLine[j + 1] = temp;
+                        }
+                        else if (order.Equals("-d"))
+                        {
+                            while ((j >= 0) && (arrayLine[j].CompareTo(temp) < 0))
+                            {
+                                arrayLine[j + 1] = arrayLine[j];
+                                j--;
+                            }
+                            arrayLine[j + 1] = temp;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Не правильно задан порядок сортировки");
+                            Console.ReadKey();
+                            return new string[] { };
+                        }
+                    }
+                    return arrayLine;
+                }
+                Console.WriteLine("Не правильно задан тип данных");
+                Console.ReadKey();
+                return null;
+            }
+            catch (Exception exeption)
+            {
+                Console.WriteLine(exeption.Message);
+                Console.ReadKey();
+                return null;
+
+            }
+        }
+        public static void WriteFile(string fileNameOut, string[] arrayLine)
+        {
+            try
+            {
                 using (StreamWriter writer = new StreamWriter(fileNameOut))
                 {
                     foreach (string s in arrayLine)
@@ -138,9 +160,9 @@ namespace Sort
             {
                 Console.WriteLine(exeption.Message);
                 Console.ReadKey();
-                return;
             }
         }
+
 
     }
 }
