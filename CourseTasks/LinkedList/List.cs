86b14fc4ae67bf77a27e3cd1bd2a1e9f631ff676
Анна Титова <lisa@ngs.ru>
2017-07-05 15:06:09 +0700

    LinkedList, создание класса Node

---
+++ CourseTasks/LinkedList/List.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class List
+    {
+
+    }
+
+    class Node<T>
+    {
+     //   private Node<T> nextNode;
+        public Node(T node)
+        {
+            nextNode = null;
+            this.node = node;
+        }
+        public T node
+        {
+            get;
+            set;
+        }
+        public Node<T> nextNode
+        {
+            get;
+            set;
+        }
+    }
+}
