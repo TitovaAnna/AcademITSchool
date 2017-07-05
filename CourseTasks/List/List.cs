f2ac396616f3fda4f22de2bed2566ce47cd94810
Анна Титова <lisa@ngs.ru>
2017-07-05 12:51:04 +0700

    Создание List

---
+++ CourseTasks/List/List.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace List
+{
+    class List
+    {
+        public static List<string> ListCreate()
+        {
+            string fileName = "1.txt";
+            List<string> arrayLine = new List<string>();
+            using (StreamReader reader = new StreamReader(fileName, Encoding.Default))
+            {
+                while (!reader.EndOfStream)
+                {
+                    arrayLine.Add(reader.ReadLine());
+                }
+            }
+            return arrayLine;
+        }
+
+        public static void DeleteEven(List<int> list)
+        {
+            list.RemoveAll(isEven);
+        }
+
+        public static IEnumerable<int> Distinct(List<int> list)
+        {
+            return list.Distinct();
+        }
+
+        private static bool isEven(int i)
+        {
+            return i % 2 == 0;
+        }
+    }
+}
---
+++ CourseTasks/List/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrayLine = List.ListCreate();
+            foreach (string s in arrayLine)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+            List<int> listInt = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                listInt.Add(i);
+            }
+
+            List.DeleteEven(listInt);
+
+            foreach (int s in listInt)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+            List<int> list = new List<int>() { 3, 4, 2, 4, 6, 7, 2, 3, 1 };
+            IEnumerable<int> distinctList = List.Distinct(list);
+
+            foreach (int s in distinctList)
+            {
+                Console.WriteLine(list.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
