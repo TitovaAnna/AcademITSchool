7a38a0c78e6188ec92725d2a1d40367729549c9c
Анна Титова <lisa@ngs.ru>
2017-07-06 17:00:01 +0700

    List, часть заданий

--- CourseTasks/LinkedList/Program.cs
+++ CourseTasks/LinkedList/Program.cs
@@ -17,26 +17,24 @@ namespace LinkedList
             list.Add(1);
             list.Add(23);
 
-            //Console.WriteLine(list.GetValue(4));
-            //Console.WriteLine(list.SetValue(4, 57));
-            //Console.WriteLine(list.GetValue(4));
-            //Console.ReadKey();
+            Console.WriteLine(list.GetValue(4));
+            Console.WriteLine(list.SetValue(4, 57));
+            Console.WriteLine(list.GetValue(4));
+            Console.ReadKey();
 
-            //Console.WriteLine(list.GetNode(3).value);
-            //Console.WriteLine(list.GetNode(3).nextNode.value);
-            //Console.ReadKey();
+            Console.WriteLine(list.GetNode(3).value);
+            Console.WriteLine(list.GetNode(3).nextNode.value);
+            Console.ReadKey();
 
-            //Console.WriteLine(list.DeleteNode(3));
-            //Console.WriteLine(list.GetNode(3).value);
-            //Console.ReadKey();
+            Console.WriteLine(list.DeleteNode(3));
+            Console.WriteLine(list.GetNode(3).value);
+            Console.ReadKey();
 
-            //list.InsertBegining(3);
-            //Console.WriteLine(list.GetFirstNode().value);
-            //Console.WriteLine(list.GetNode(0).value);
-            //Console.ReadKey();
+            list.InsertBegining(3);
+            Console.WriteLine(list.GetFirstNode().value);
+            Console.WriteLine(list.GetNode(0).value);
+            Console.ReadKey();
 
-            list.DeleteNodeValue(9);
-          
             Console.WriteLine(list.GetNode(0).value);
             Console.WriteLine(list.GetNode(1).value);
             Console.WriteLine(list.GetNode(2).value);
