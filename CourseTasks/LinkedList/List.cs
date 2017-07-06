c45f9ac62e48220ba92eb90d8702b352e3068013
Анна Титова <lisa@ngs.ru>
2017-07-06 16:58:44 +0700

    List, часть заданий.

--- CourseTasks/LinkedList/List.cs
+++ CourseTasks/LinkedList/List.cs
@@ -6,20 +6,180 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    class List
+    class LinkedList<T>
     {
+        private Node<T> head = null;
+        private Node<T> current = null;
+        private int size = 0;
 
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public void Add(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            size++;
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                current.nextNode = node;
+            }
+            current = node;
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public Node<T> GetFirstNode()
+        {
+            Node<T> firstNode = new Node<T>(head.value);
+            return firstNode;
+        }
+
+        public T GetValue(int index)
+        {
+            if (index >= Size)
+            {
+                throw new ArgumentOutOfRangeException("Индекс больше количества узлов");
+            }
+            int i = 0;
+            Node<T> node = new Node<T>(head.value, head.nextNode);
+            while (i < index)
+            {
+                node = node.nextNode;
+                i++;
+            }
+            return node.value;
+        }
+
+        public T SetValue(int index, T valueNew)
+        {
+            if (index >= Size)
+            {
+                throw new ArgumentOutOfRangeException("Индекс больше количества узлов");
+            }
+            int i = 0;
+            Node<T> node = new Node<T>(head.value, head.nextNode);
+
+            while (i < index)
+            {
+                node = node.nextNode;
+                i++;
+            }
+            T valueOld = node.value;
+            node.value = valueNew;
+            return valueOld;
+        }
+
+        public Node<T> GetNode(int index)
+        {
+            if (index >= Size)
+            {
+                throw new ArgumentOutOfRangeException("Индекс больше количества узлов");
+            }
+            int i = 0;
+            Node<T> node = new Node<T>(head.value, head.nextNode);
+            while (i < index)
+            {
+                node = node.nextNode;
+                i++;
+            }
+            return node;
+        }
+
+        public T DeleteNode(int index)
+        {
+            if (index >= Size)
+            {
+                throw new ArgumentOutOfRangeException("Индекс больше,чем количество узлов");
+            }
+            T valueOld = GetNode(index).value;
+
+            if (index == 0)
+            {
+                head = GetNode(1);
+                return valueOld;
+            }
+
+            GetNode(index - 1).nextNode = GetNode(index + 1);
+            size--;
+            return valueOld;
+        }
+
+        public void InsertBegining(T value)
+        {
+            Node<T> headNew = new Node<T>(value, head);
+            head = headNew;
+            size++;
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index > Size)
+            {
+                throw new ArgumentOutOfRangeException("Индекс больше,чем количество узлов");
+            }
+
+            if (index == Size)
+            {
+                Node<T> nodeNew = new Node<T>(value, null);
+                GetNode(index - 1).nextNode = nodeNew;
+            }
+            else if (index == 1)
+            {
+                Node<T> nodeNew = new Node<T>(value, GetNode(index));
+                head.nextNode = nodeNew;
+            }
+            else if (index == 0)
+            {
+                InsertBegining(value);
+            }
+            else
+            {
+                Node<T> nodeNew = new Node<T>(value, GetNode(index));
+                GetNode(index - 1).nextNode = nodeNew;
+            }
+            size++;
+        }
+
+        //public void DeleteNodeValue(T value)
+        //{
+        //    for (int i = 0; i < size; i++)
+        //    {
+        //        if (GetNode(i).value.Equals(value))
+        //        {
+        //            DeleteNode(i);
+        //            // size--;
+        //        }
+        //    }
+        //}
     }
 
     class Node<T>
     {
-     //   private Node<T> nextNode;
-        public Node(T node)
+        public Node(T value)
         {
             nextNode = null;
-            this.node = node;
+            this.value = value;
         }
-        public T node
+
+        public Node(T value, Node<T> nextNode)
+        {
+            this.nextNode = nextNode;
+            this.value = value;
+        }
+
+        public T value
         {
             get;
             set;
--- CourseTasks/LinkedList/Program.cs
+++ CourseTasks/LinkedList/Program.cs
@@ -10,6 +10,40 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+            LinkedList.LinkedList<int> list = new LinkedList<int>();
+            list.Add(9);
+            list.Add(7);
+            list.Add(9);
+            list.Add(1);
+            list.Add(23);
+
+            //Console.WriteLine(list.GetValue(4));
+            //Console.WriteLine(list.SetValue(4, 57));
+            //Console.WriteLine(list.GetValue(4));
+            //Console.ReadKey();
+
+            //Console.WriteLine(list.GetNode(3).value);
+            //Console.WriteLine(list.GetNode(3).nextNode.value);
+            //Console.ReadKey();
+
+            //Console.WriteLine(list.DeleteNode(3));
+            //Console.WriteLine(list.GetNode(3).value);
+            //Console.ReadKey();
+
+            //list.InsertBegining(3);
+            //Console.WriteLine(list.GetFirstNode().value);
+            //Console.WriteLine(list.GetNode(0).value);
+            //Console.ReadKey();
+
+            list.DeleteNodeValue(9);
+          
+            Console.WriteLine(list.GetNode(0).value);
+            Console.WriteLine(list.GetNode(1).value);
+            Console.WriteLine(list.GetNode(2).value);
+            Console.WriteLine(list.GetNode(3).value);
+            Console.ReadKey();
+
+
         }
     }
 }
