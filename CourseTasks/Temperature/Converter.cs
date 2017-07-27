2d13a79b5faa8e2d1d48d03476f7be46b1fa631e
Анна Титова <lisa@ngs.ru>
2017-07-27 23:33:19 +0700

    Перевод температуры добавлена

---
+++ CourseTasks/Temperature/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature
+{
+    class Converter
+    {
+        public static double Convert(Scales.Scale scaleIn, Scales.Scale scaleOut, double value)
+        {
+            double temp = Scales.TransferToC(scaleIn, value);
+            return Scales.TransferFromC(scaleOut, temp);
+        }
+    }
+}
---
+++ CourseTasks/Temperature/Scales.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature
+{
+    class Scales
+    {
+        public enum Scale
+        {
+            Celsium = 0,
+            Kelvin = 1,
+            Fahrenheit = 2
+        }
+        public static double TransferToC(Scale scale, double valueIn)
+        {
+            if (scale == Scale.Fahrenheit)
+            {
+                return (valueIn - 32) * 5 / 9;
+            }
+            else if (scale == Scale.Kelvin)
+            {
+                return valueIn - 273.15;
+            }
+            return valueIn;
+        }
+        public static double TransferFromC(Scale scale, double valueIn)
+        {
+            if (scale == Scale.Fahrenheit)
+            {
+                return valueIn * 9 / 5 + 32;
+            }
+            else if (scale == Scale.Kelvin)
+            {
+                return valueIn + 273.15;
+            }
+            return valueIn;
+        }
+    }
+}
